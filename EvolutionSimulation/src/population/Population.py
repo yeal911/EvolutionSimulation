2ec76a80edc1c700c13d9353f63527e34d5366ed
yeal911 <yeal91117@gmail.com>
2021-03-15 23:06:49 +0100

    add population class add wolf class

---
+++ EvolutionSimulation/src/population/Population.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from abc import ABCMeta, abstractmethod
+
+
+class Population(metaclass=ABCMeta):
+    """this abstract class defines the common actions of population"""
+
+    # forage behaviour of a population
+    @abstractmethod
+    def forage(self): pass
+
+    # grow behaviour of a population
+    @abstractmethod
+    def grow(self): pass
+
+    # attack behaviour of a population
+    @abstractmethod
+    def attack(self): pass
+
+    # defend behaviour of a population
+    @abstractmethod
+    def defend(self): pass
+
+    # breed behaviour of a population
+    @abstractmethod
+    def breed(self): pass
---
+++ EvolutionSimulation/src/population/Wolf.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import Population
+
+
+class Wolf(Population):
+    """this class defines the properties and behaviours of wolf population"""
+
+    # lifespan
+    lifespan = None
+
+    # gender
+    gender = None
+
+    # hungry level
+    hungryLevel = None
+
+    # gene set
+    geneSet = []
+
+    # breeding times for each individual
+    breedingTimes = 2
+
+    # attack possibility
+    attackPossibility = 8
+
+    # defend possibility
+    defendPossibility = 8
+
+    # fight capability
+    fightCapability = 8
+
+    @classmethod  # static method of a class
+    def populationName(cls):
+        return "Wolf"
+
+    def __init__(self, gene_set):
+        self.geneSet = gene_set
+        # add computation for properties based on gene set
+
+    # forage behaviour of wolf
+    def forage(self):
+
+
+    # grow behaviour of a wolf
+    def grow(self):
+
+
+    # attack behaviour of a wolf
+    def attack(self):
+
+
+    # defend behaviour of a wolf
+    def defend(self):
+
+
+    # breed behaviour of a wolf
+    def breed(self):
+        
+

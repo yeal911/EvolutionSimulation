d997bea44a92a0c9c00cf7d4033e8e71e285290d
yeal911 <yeal91117@gmail.com>
2021-03-31 16:20:34 +0200

    separate class Wolf and WolfThread

--- EvolutionSimulation/src/Wolf.py
+++ EvolutionSimulation/src/Wolf.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 import math
 import random
-import threading
 import time
 
-from EvolutionSimulation.src.Dreamland import Dreamland
-from EvolutionSimulation.src.PopulationThread import PopulationThread
 from Population import Population
 from Gene import Gene
 
@@ -162,94 +159,3 @@ class Wolf(Population):
                 childGeneDigits.append(parentY.gene.geneDigits[i+1])
             return Wolf(Gene(childGeneDigits), round((self.generation + spouse.generation) / 2))
         return None
-
-
-class WolfThread(threading.Thread, PopulationThread):
-    """
-    Thread for wolf population, to evolute all the individuals of wolf population
-
-    ***parameter explanation***
-    dreamland: where this thread runs
-    wolfCount: initiate the wolf count to be loaded to dream land
-    coordinateX: x coordinate in dream land
-    coordinateY: y coordinate in dream land
-    slotCode: slot code computed
-    wolves: all the individuals
-
-    """
-
-    # initialize wolf thread
-    def __init__(self, wolf_count, dreamland: Dreamland):
-        threading.Thread.__init__(self)
-        self.dreamland = dreamland
-        self.wolfCount = wolf_count
-        self.wolves = []
-        self.deadWolves = []
-        for i in range(0, wolf_count):
-            # need to randomly initialize the coordinates of the wolf
-            wolf = Wolf()
-            wolf.coordinateX = random.randint(0, Dreamland.SIZE_X)
-            wolf.coordinateY = random.randint(0, Dreamland.SIZE_Y)
-            # set the slot code in the dreamland
-            wolf.slotCode = Dreamland.returnSlotNo(wolf.coordinateX, wolf.coordinateY)
-            self.wolves.append(wolf)
-            # update coordinate map
-            self.updateDreamLandMap(wolf, None, wolf.slotCode)
-        # add wolf thread to dreamland
-        # dreamland.populationPlayers.append(self)
-
-    # monitor all wolves, and execute for all their actions
-    def run(self):
-        # sleep for 1 day (1s)
-        time.sleep(1)
-        for wolf in self.wolves:
-            # add logic for forage, find for food if it is hungry
-            if wolf.hungryLevel > 5:
-                # find food in its own slot, if there is, then flight, if none, change position
-                print(wolf.name + " hungryLevel: " + str(wolf.hungryLevel))
-            else:
-                print(wolf.name + " hungryLevel: " + str(wolf.hungryLevel))
-            # add logic for grow
-
-            # add logic for flight
-
-            # add logic for breed
-
-    # # update coordinate map after individual's location changing
-    # def updateDreamLandMap(self, individual: Population, original, target):
-    #     if original is not None:
-    #         originalSlot = self.dreamland.coordinateMap[original]
-    #         originalSlot.remove(individual)
-    #     targetSlot = self.dreamland.coordinateMap[target]
-    #     targetSlot.append(individual)
-    #
-    # # move individual location
-    # def moveLocation(self, individual: Wolf):
-    #     moveDirection = random.randint(1, 4)
-    #     originalSlot = individual.slotCode
-    #     # move east
-    #     if moveDirection == 1:
-    #         individual.coordinateX += 10
-    #         if individual.coordinateX > Dreamland.SIZE_X:
-    #             individual.coordinateX = Dreamland.SIZE_X
-    #     # move south
-    #     elif moveDirection == 2:
-    #         individual.coordinateY -= 10
-    #         if individual.coordinateY < 0:
-    #             individual.coordinateY = 0
-    #     # move west
-    #     elif moveDirection == 3:
-    #         individual.coordinateX -= 10
-    #         if individual.coordinateX < 0:
-    #             individual.coordinateX = 0
-    #     # move north
-    #     else:
-    #         individual.coordinateY += 10
-    #         if individual.coordinateY > Dreamland.SIZE_Y:
-    #             individual.coordinateY = Dreamland.SIZE_Y
-    #     individual.slotCode = Dreamland.returnSlotNo(individual.coordinateX, individual.coordinateY)
-    #     self.updateDreamLandMap(individual, originalSlot, individual.slotCode)
-    #
-    # # search food in near 2 slots from 4 directions
-    # def searchFood(self, individual: Wolf):
-    #     targetFood = None
